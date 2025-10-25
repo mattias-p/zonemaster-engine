80ebecb8e081d5dfec3274ac92ff070c7ad053bc
Mattias Päivärinta <mattias.paivarinta@iis.se>
2025-10-25 20:37:40 +0200

    Refactor

--- t/Mock/Scripted.pm
+++ t/Mock/Scripted.pm
@@ -1,4 +1,4 @@
-package Mock::Behavior;
+package Mock::Scripted;
 use v5.26;
 use warnings;
 
@@ -17,35 +17,36 @@ sub new {
     return bless $obj, $class;
 }
 
-sub append_script {
-    my ( $self, @script ) = @_;
+sub expect_call {
+    my ( $self, $expectation ) = @_;
 
-    for my $i ( 0 .. $#script ) {
-        eval { _validate_expected( $script[$i] ) };
-        if ( $@ ) {
-            die "position $i: $@";    # rethrow
-        }
-    }
+    _validate_expectation( $expectation );
 
-    push $self->@*, @script;
+    push $self->@*, $expectation;
 
     return $self;
 }
 
-sub ok_done {
+sub reset {
+    my ( $self ) = @_;
+
+    $self->@* = ();
+}
+
+sub verify_done {
     my ( $self, $name ) = @_;
 
     $name //= 'no more calls expected';
 
     $TB->ok( scalar( $self->@* ) == 0, $name );
 
-    for my $expected ( $self->@* ) {
-        my ( $exp_method, $exp_args, $code ) = $expected->@{qw( method args code )};
+    for my $expectation ( $self->@* ) {
+        my ( $exp_method, $exp_args, $do ) = $expectation->@{qw( method args do )};
         my %exp = (
             method => $exp_method,
             args   => $exp_args,
         );
-        $TB->diag( "leftover: " . ( $expected->{name} // "call to '$exp_method'" ) );
+        $TB->diag( "leftover: " . ( $expectation->{name} // "call to '$exp_method'" ) );
         local $Data::Dumper::Purity   = 0;
         local $Data::Dumper::Sortkeys = 1;
         local $Data::Dumper::Terse    = 1;
@@ -53,7 +54,7 @@ sub ok_done {
     }
 
     return !$self->@*;
-} ## end sub ok_done
+} ## end sub verify_done
 
 sub AUTOLOAD {
     my ( $self, @args ) = @_;
@@ -63,48 +64,61 @@ sub AUTOLOAD {
         return;
     }
 
-    my $expected = shift( $self->@* ) // {};
-    my ( $name, $exp_method, $exp_args, $code ) = $expected->@{qw( name method args code )};
-    $exp_method //= '<none>';
-    $name       //= "call to '$exp_method' with expected args";
-
     my $got_method = $AUTOLOAD =~ s/^.*:://r;
-
-    my @got = (
+    my %got        = (
         method => $got_method,
         args   => \@args,
     );
-    my @exp =
-      $expected
-      ? (
+
+    my $expectation = shift( $self->@* );
+    if ( !$expectation ) {
+        $TB->ok( 0, 'no more calls expected, got ' . $got_method );
+        $TB->diag( Dumper( \%got ) );
+        confess "unexpected call to '$got_method'";
+    }
+
+    my ( $name, $exp_method, $exp_args, $do, $returns ) = $expectation->@{qw( name method args do returns )};
+    my %exp = (
         method => $exp_method,
         args   => $exp_args,
-      )
-      : ();
+    );
+
+    $name //= "call to '$exp_method' with expected args";
 
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    my $ok = eq_deeply( \@got, \@exp );
+    my $ok = eq_deeply( \%got, \%exp );
 
     $TB->ok( $ok, $name );
     if ( !$ok ) {
         local $Data::Dumper::Purity   = 0;
         local $Data::Dumper::Sortkeys = 1;
         local $Data::Dumper::Terse    = 1;
-        $TB->diag( Dumper( \@got, \@exp ) );
+        $TB->diag( Dumper( \%got, \%exp ) );
         confess "unexpected call to '$got_method'";
     }
 
-    return $code->( @args );
+    if ( $do ) {
+        return $do->( @args );
+    }
+    else {
+        return $returns;
+    }
 } ## end sub AUTOLOAD
 
-sub _validate_expected {
-    my ( $expected ) = @_;
+sub _validate_expectation {
+    my ( $expectation ) = @_;
 
-    if ( ref $expected ne 'HASH' ) {
+    if ( ref $expectation ne 'HASH' ) {
         confess 'script expectation must be a hashref';
     }
-    my %exp = $expected->%*;
-    my ( $name, $method, $args, $code ) = delete @exp{qw( name method args code )};
+
+    my @impl = grep { exists $expectation->{$_} } qw( do returns );
+    if ( @impl != 1 ) {
+        confess 'exactly one of the following fields must be specified: do, returns';
+    }
+
+    my %exp = $expectation->%*;
+    my ( $name, $method, $args, $do, $returns ) = delete @exp{qw( name method args do returns )};
     if ( %exp ) {
         confess 'unrecognized expectation fields: ' . join( ' ', sort keys %exp );
     }
@@ -121,11 +135,11 @@ sub _validate_expected {
         confess 'args field must be an arrayref, got ' . ( ref( $args ) || 'a scalar' );
     }
 
-    if ( ref $code ne 'CODE' ) {
-        confess 'code field must be a coderef, got ' . ( ref( $args ) || 'a scalar' );
+    if ( $impl[0] eq 'do' && ref $do ne 'CODE' ) {
+        confess 'do field must be a coderef, got ' . ( ref( $do ) || 'a scalar' );
     }
 
     return;
-} ## end sub _validate_expected
+} ## end sub _validate_expectation
 
 1;
